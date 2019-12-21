a1b5bff7002db9e5638174f6356a23e9f3f0f05f
Poscan <doodark13@gmail.com>
2019-12-21 14:17:06 +0500

    Button

--- app/src/main/java/com/example/xaxaxakaton/MainActivity.java
+++ app/src/main/java/com/example/xaxaxakaton/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
+
         @Override
         public void onMapReady(GoogleMap googleMap) {
             map =googleMap;
